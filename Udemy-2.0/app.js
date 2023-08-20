d0bc33e7349b97dfa018c6c172eb77f61bf7dd94
Man29toconas <toconascruz@gmail.com>
2023-08-20 16:29:42 -0300

    finished

---
+++ Udemy-2.0/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 3001
+const app = express();
+
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/home.html"))
+})
+
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/register.html"))
+})
+
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"))
+})
+
+app.listen(port, () => {
+  console.log(`Servidor levantado en http://localhost:${port} `);
+})
